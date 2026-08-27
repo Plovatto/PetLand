497b5fd5473f9e1d0fd536afa9ccb31b63c93c58
Plovatto <pandora-blovatto@educar.rs.gov.br>
2026-08-27 14:28:39 -0300

    :sparkles: feat: add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Header from '@/components/layout/Header'
+import Hero from '@/components/sections/Hero'
 
 function App() {
   return (
     <div className="bg-background min-h-screen">
       <Header />
 
-      <main className="flex min-h-screen items-center justify-center bg-white">
-        <p className="text-lg text-neutral-500">PetLand — base do projeto pronta.</p>
+      <main>
+        <Hero />
       </main>
     </div>
   )
